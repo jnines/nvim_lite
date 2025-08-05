1ac24ab16b870614dabae72e5bbde70d4f25a180
jnines <jason.mullenberg@gmail.com>
2025-08-05 13:42:09 -0500

    Add ft for plugin

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,5 +1,6 @@
 return {
 	"windwp/nvim-ts-autotag",
+	ft = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
 	config = function()
 		local autotag = require("nvim-ts-autotag")
 		autotag.setup({})
